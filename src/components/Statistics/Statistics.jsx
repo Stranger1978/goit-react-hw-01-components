f37f2dd2e4ae36cbf8e4049d7cea36fc0ac432d4
Pavlo Yeromenko <108014692+Stranger1978@users.noreply.github.com>
2023-07-26 11:31:16 +0300

    styled second task

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import style from './Statistics.module.css';
 
 const Statistics = ({ title, stats }) => {
     return (
-        <section class="statistics">
-            {title && <h2 class="title">{title}</h2>}
-            <ul class="stat-list">
+        <section class={style.statistics}>
+            {title && <h2 class={style.title}>{title}</h2>}
+            <ul class={style.stat_list}>
                 {stats.map(({ id, label, percentage }) => {
                     return (
-                        <li class="item" id={id}>
+                        <li class={style.item} id={id} style={{ backgroundColor: getRandomHexColor() }}>
                         <span class="label">{label}</span>
-                        <span class="percentage">{percentage}%</span>
+                            <span class={style.item_percentage}>{percentage}%</span>
                     </li>)
                 })}
             </ul>
@@ -22,6 +23,10 @@ Statistics.propTypes = {
 title: PropTypes.string,
 label: PropTypes.string,
 percentage: PropTypes.number,
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
 export default Statistics;
